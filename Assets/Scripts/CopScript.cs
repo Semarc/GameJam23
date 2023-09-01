997f8a6f4e1aabd1eeb28a929047eebe7296b3cc
Semellie <eleodie.krusche@protonmail.com>
2023-09-01 17:19:39 +0200

    Basic Player and Enemy Movement

---
+++ Assets/Scripts/CopScript.cs
@@ -0,0 +1,77 @@
+using Pathfinding;
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Seeker))]
+public class CopScript : MonoBehaviour
+{
+	Rigidbody2D rb;
+	Seeker seeker;
+
+	[SerializeField] private Transform target;
+	[SerializeField] private float CopSpeed = 300f;
+	[SerializeField] private float nextWaypointDistance = 3f;
+
+	private Path path;
+	private int currentWaypoint = 0;
+	private bool reachedEndOfPath = false;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		seeker = GetComponent<Seeker>();
+
+		InvokeRepeating(nameof(UpdatePath), 0, 1f);
+	}
+
+	private void UpdatePath()
+	{
+		if (seeker.IsDone())
+			seeker.StartPath(rb.position, target.position, OnPathComplete);
+	}
+
+	private void OnPathComplete(Path p)
+	{
+		if (!p.error)
+		{
+			path = p;
+			currentWaypoint = 0;
+		}
+		else
+		{
+			Debug.Log("Error");
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		Debug.Log("Fixed");
+		if (path == null)
+		{
+			Debug.Log("No Path");
+			return;
+		}
+		if (currentWaypoint >= path.vectorPath.Count)
+		{
+			Debug.Log("EndOfPath");
+			reachedEndOfPath = true;
+			return;
+		}
+		else
+		{
+			reachedEndOfPath = false;
+		}
+
+		Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+		Vector2 force = direction * CopSpeed * Time.fixedDeltaTime;
+
+		rb.AddForce(force);
+		Debug.Log("Force added");
+
+		float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+		if (distance < nextWaypointDistance)
+		{
+			currentWaypoint++;
+		}
+	}
+}
