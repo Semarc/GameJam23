332a9602fca36786545b0b3b5896dad12a39f358
schmisowy <mirasophia.schwiertz@gmail.com>
2023-09-04 14:46:46 +0200

    adjusted basic edge colliders on map

--- Assets/Scripts/SimplePlayerMovement.cs
+++ Assets/Scripts/SimplePlayerMovement.cs
@@ -8,7 +8,6 @@ public class SimplePlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
     public Rigidbody2D rigidbody;
-    public Animator animator;
 
     Vector2 movement;
 
@@ -17,9 +16,6 @@ public class SimplePlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vrtiacl", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
