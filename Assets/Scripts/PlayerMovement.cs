6ff1da4496f0afa575496fe7a48f47dfc1885c1d
Tan Dahmen <tanja.dahmen@rub.de>
2023-09-04 16:34:46 +0200

    did animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
 	public Rigidbody2D rb { get; private set; }
 	private Vector2 moveDirection;
-
+	[SerializeField] Animator[] animator;
 	[SerializeField] private float speed = 400f;
 
 	void Awake()
@@ -17,7 +19,11 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-
+		float movementX = moveDirection.x;
+		float movementY = moveDirection.y;
+		animator[0].SetFloat("Horizontal", movementX);
+		animator[0].SetFloat("Vertical", movementY);
+		animator[0].SetFloat("Speed", moveDirection.magnitude);
 	}
 
 	private void OnMovement(InputValue movementValue)
