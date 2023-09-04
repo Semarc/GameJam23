dc52aefa77cd8fc8730c38d5a86f35df674a5e9c
Tan Dahmen <tanja.dahmen@rub.de>
2023-09-04 22:30:16 +0200

    did the rest of the audio

---
+++ Assets/introBGM.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introBGM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioScript.Instance.PlayMusic(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
