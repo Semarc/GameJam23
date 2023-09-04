2923eefd15f4762cd04bef1585108af1e166669a
schmisowy <mirasophia.schwiertz@gmail.com>
2023-09-04 10:08:23 +0200

    had same errors again and deleted stuff

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed;
+    public Rigidbody2D rigidbody;
+
+    void Update()
+    {
+        
+    }
+}
